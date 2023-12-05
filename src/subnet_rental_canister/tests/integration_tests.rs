95efff54d0a34f2d4e01342bcf7b77997a9c87e1
Elias Datler <elias.datler@dfinity.org>
2023-12-05 18:43:34 +0100

    add files

---
+++ src/subnet_rental_canister/tests/integration_tests.rs
@@ -0,0 +1,29 @@
+use candid::{decode_one, encode_one, Principal};
+use pocket_ic::{PocketIc, WasmResult};
+use std::fs;
+
+const WASM: &str = "../../subnet_rental_canister.wasm";
+
+#[test]
+fn test_call_greet() {
+    let pic = PocketIc::new();
+    let canister_id = pic.create_canister();
+    let wasm = fs::read(WASM).expect("Please build the wasm first");
+
+    pic.add_cycles(canister_id, 2_000_000_000_000);
+    pic.install_canister(canister_id, wasm, vec![], None);
+
+    let WasmResult::Reply(res) = pic
+        .query_call(
+            canister_id,
+            Principal::anonymous(),
+            "greet",
+            encode_one("SRC").unwrap(),
+        )
+        .unwrap()
+    else {
+        panic!("Expected a reply")
+    };
+
+    println!("Reply: {:?}", decode_one::<String>(&res).unwrap());
+}
