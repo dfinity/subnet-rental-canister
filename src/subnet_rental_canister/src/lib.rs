4461e4c462506d55c16ce85cbc3ac1aecb1b580e
Michael Weigelt <michael.weigelt@dfinity.com>
2023-12-06 16:09:07 +0000

    wip

--- src/subnet_rental_canister/src/lib.rs
+++ src/subnet_rental_canister/src/lib.rs
@@ -1,4 +1,49 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use candid::Principal;
+use ic_cdk::api::call;
+use std::cell::RefCell;
+use std::collections::HashMap;
+
+type SubnetId = Principal;
+
+#[derive(Clone, Copy)]
+struct RentalConditions {
+    daily_cost_e8s: u64,
+    minimal_rental_period_days: u64,
+}
+
+thread_local! {
+    static RENTAL_CONDITIONS: RefCell<HashMap<SubnetId, RentalConditions>> = RefCell::new(HashMap::new());
+}
+
+pub enum ExecuteProposalError {
+    Failure(String),
+}
+
+/// Argument will be ValidatedSRProposal, created by government canister via
+/// SRProposal::validate().
+async fn on_proposal_accept(
+    subnet_id: SubnetId,
+    principals: Vec<Principal>,
+    block_index: usize,
+    refund_address: String,set_authorized_subnetwork_list
+) -> Result<(), ExecuteProposalError> {
+    // assumptions:
+    // - a single deposit transaction exists and covers amount
+
+    // whitelist principal
+    let result: CallResult<(_,)> =
+        call(Principal::from_text("cmc_id").unwrap(), "", ()).await;
+
+    // collect rental information
+    let RentalConditions {
+        daily_cost_e8s,
+        minimal_rental_period_days,
+    } = RENTAL_CONDITIONS.with(|map| map.borrow().get(&subnet_id).unwrap().clone());
+
+    // cost of initial period:
+    let initial_cost_e8s = daily_cost_e8s * minimal_rental_period_days; // what about overflows
+    // turn this amount of ICP into cycles and burn them. 
+
+
+    Ok(())
 }
