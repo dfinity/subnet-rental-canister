17701af36ebf2719768e3f9fbf71ab81980e8d8b
Michael Weigelt <michael.weigelt@dfinity.com>
2024-04-04 07:31:30 +0000

    dependencies

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,9 @@ cd "$SCRIPTS_DIR/.."
 
 # Build canister
 export RUSTFLAGS="--remap-path-prefix $(readlink -f $(dirname ${0}))=/build --remap-path-prefix ${CARGO_HOME}=/cargo"
-cargo rustc -p subnet_rental_canister --crate-type=cdylib --locked --target wasm32-unknown-unknown --release
+cargo rustc -p subnet_rental_canister --crate-type=cdylib --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/subnet_rental_canister.wasm subnet_rental_canister.wasm
+
+cargo rustc -p xrc_mock --crate-type=cdylib --target wasm32-unknown-unknown --release
+cp target/wasm32-unknown-unknown/release/xrc_mock.wasm src/subnet_rental_canister/tests/exchange-rate-canister.wasm
+gzip src/subnet_rental_canister/tests/exchange-rate-canister.wasm 
