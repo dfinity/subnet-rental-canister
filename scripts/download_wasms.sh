2949fab1c4086c2700ddc5556fd28395425fc895
Elias Datler <elias.datler@dfinity.org>
2025-06-19 19:54:47 +0200

    Bump

--- scripts/download_wasms.sh
+++ scripts/download_wasms.sh
@@ -4,6 +4,6 @@
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPTS_DIR/.."
 
-COMMIT="41805f62cb4758ca30adec9bc4af936cf700e968"
+COMMIT="ac7ff452684f84ea0cfc3fd0a27228220a368b33" # Jun 17, 2025
 DESTINATION_DIR="src/subnet_rental_canister/tests"
 curl --output-dir $DESTINATION_DIR -sLO https://download.dfinity.systems/ic/$COMMIT/canisters/ledger-canister.wasm.gz
