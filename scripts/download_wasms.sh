6343775fea99f92fb43200ed88260fbe307bd31c
Elias Datler <elias.datler@dfinity.org>
2025-06-23 13:09:49 +0200

    Download wasm from CI

--- scripts/download_wasms.sh
+++ scripts/download_wasms.sh
@@ -7,3 +7,6 @@ cd "$SCRIPTS_DIR/.."
 COMMIT="ac7ff452684f84ea0cfc3fd0a27228220a368b33" # Jun 17, 2025
 DESTINATION_DIR="src/subnet_rental_canister/tests"
 curl --output-dir $DESTINATION_DIR -sLO https://download.dfinity.systems/ic/$COMMIT/canisters/ledger-canister.wasm.gz
+
+COMMIT="91475808ac57f204831933295557051201196a7c" # Jun 23, 2025 (PR #5652, from 'Bazel test all' Checkout step)
+curl --output-dir $DESTINATION_DIR -sLO https://download.dfinity.systems/ic/$COMMIT/canisters/cycles-minting-canister.wasm.gz
