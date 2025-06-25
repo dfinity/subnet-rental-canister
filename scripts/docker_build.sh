644cdf0fa65779effaa2f1056ca27ca26abf484a
Elias Datler <elias.datler@dfinity.org>
2025-06-25 23:22:06 +0200

    Fix

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -9,7 +9,7 @@ cd "$SCRIPTS_DIR/.."
 # Build the Docker image and run it to build the canister
 docker build -t localhost/subnet-rental-canister .
 # Run the container and execute dfx build inside it
-docker run --name subnet-rental-canister-build --platform="linux/amd64" --user "$(id -u):$(id -g)" -v "$(pwd):/app" localhost/subnet-rental-canister bash -c "dfx build --ic"
+docker run --name subnet-rental-canister-build --platform="linux/amd64" -v "$(pwd):/app" localhost/subnet-rental-canister bash -c "dfx build --ic"
 # Copy the Wasm file to the root directory
 docker cp subnet-rental-canister-build:/app/target/wasm32-unknown-unknown/release/subnet_rental_canister.wasm ./subnet_rental_canister.wasm
 # Cleanup
