85f9d82817cf40caf25c522b2f1240ab383f3149
Elias Datler <eliasdatler@me.com>
2025-07-01 22:37:48 +0200

    Better image name

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -7,8 +7,8 @@ SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPTS_DIR/.."
 
 # Build the Docker image
-docker build --platform linux/amd64 -t subnet-rental-canister .
+docker build --platform linux/amd64 -t src-builder .
 # Run the container and build the subnet rental canister inside it
-docker run --rm -v "$(pwd):/app" subnet-rental-canister bash -c "dfx build --ic"
+docker run --rm -v "$(pwd):/app" src-builder bash -c "dfx build --ic"
 # Copy the Wasm file to the root directory
 cp .dfx/ic/canisters/subnet_rental_canister/subnet_rental_canister.wasm.gz ./subnet_rental_canister.wasm.gz
